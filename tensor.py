acc9552080d0e8376148a3fc79390dc3231f9d08
neilalden <escobin.neil@gmail.com>
2020-04-08 21:45:29 +0800

    first machine learning test

---
+++ tensor.py
@@ -0,0 +1,18 @@
+import tensorflow
+import tensorflow.keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3","studytime","absences", "failures","traveltime"]]
+
+predict = "G3"
+
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
