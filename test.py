d8d33e42ff46cc2e76c3042c58d7eac951530663
neilalden <escobin.neil@gmail.com>
2020-04-09 07:41:28 +0800

    linear model with saved highest accuracy model and plotted data

--- test.py
+++ test.py
@@ -5,22 +5,36 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
-data = data[["G1", "G2", "G3","studytime","absences", "failures","traveltime"]]
+data = data[["G1", "G2", "G3", "studytime", "absences", "failures", "traveltime"]]
 
 predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+'''
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train,y_train)
-accuracy = linear.score(x_test,y_test)
-print(accuracy)
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
+
+    if accuracy > best:
+        best = accuracy
+        with open("studentModel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+'''
+pickle_rick = open("studentModel.pickle", "rb")
+linear = pickle.load(pickle_rick)
 
 print("co: ", linear.coef_)
 print("intercept: ", linear.intercept_)
@@ -29,3 +43,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = 'absences'
+style.use("ggplot")
+pyplot.scatter(data[p],data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
